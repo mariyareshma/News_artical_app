4c85eb6a5280b5623cf9a6a799827c6da6006288
Mariya Reshma <rreshma10653@gmail.com>
2023-02-24 10:38:37 +0530

    To change the services file

--- lib/service/services.dart
+++ lib/service/services.dart
@@ -4,18 +4,14 @@ import '../model/news_article.dart';
 
 var dio = Dio();
 
-Future<List<NewsArticle>> getNewsArticleDetails() async {
+Future<List<NewsArticle>> getPersonDetails() async {
   var response = await dio.get(
       'http://api.mediastack.com/v1/news?access_key=d1b4b8118b06b2530858b42668464993&countries=in&limits=100');
 
   if (response.statusCode == 200) {
     var newsJsons = response.data;
-    var news = <NewsArticle>[];
-    for (var newsJson in newsJsons) {
-      var newsObj = NewsArticle.fromJson(newsJson);
-      news.add(newsObj);
-    }
-    return news;
+    var newArticleObj = NewsArticles.fromJson(newsJsons);
+    return newArticleObj.data;
   }
 
   return <NewsArticle>[];
